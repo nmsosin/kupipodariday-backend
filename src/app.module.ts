a0250aeb8e785ed2c7eda53207a1955723bb4257
Nikolay Sosin <nmsosin@yandex.ru>
2023-12-26 19:43:40 +0300

    Add main entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
+import { User } from './users/entities/user.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { Wishlist } from './wishlists/entities/wishlist.entity';
+import { Offer } from './offers/entities/offer.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'nest_project',
+      entities: [User, Wish, Wishlist, Offer],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User, Wish, Wishlist, Offer]),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
