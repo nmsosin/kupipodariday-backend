85a512404b6cc8b431da2215af22098d49759289
Nikolay Sosin <nmsosin@yandex.ru>
2024-02-29 14:02:26 +0300

    feat: add users service & DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString, IsUrl, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(2, 30)
+  username: string;
+
+  @IsString()
+  @IsUrl()
+  @IsOptional()
+  avatar: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  @Length(2, 200)
+  about: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    if (createUserDto.username) {
+      const usernameExist = await this.findOneByUsername(
+        createUserDto.username,
+      );
+      if (usernameExist) {
+        throw new ConflictException(EXCEPTIONS.Conflict);
+      }
+    }
+
+    if (createUserDto.email) {
+      const emailExist = await this.findOneByEmail(createUserDto.email);
+      if (emailExist) {
+        throw new ConflictException(EXCEPTIONS.Conflict);
+      }
+    }
+
+    const password = await bcrypt.hash(createUserDto.password, 8);
+
+    const newUser = await this.userRepository.create({
+      ...createUserDto,
+      password: password,
+    });
+    return await this.userRepository.save(newUser);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return await this.userRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOneByUsername(username: string) {
+    return await this.userRepository.findOneBy({ username });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOneByEmail(email: string) {
+    return await this.userRepository.findOneBy({ email });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const currentUser = await this.findOne(id);
+
+    if (updateUserDto.username) {
+      const usernameExist = await this.findOneByUsername(
+        updateUserDto.username,
+      );
+      if (usernameExist) {
+        throw new ConflictException(EXCEPTIONS.Conflict);
+      }
+    }
+
+    if (updateUserDto.email) {
+      const emailExist = await this.findOneByEmail(updateUserDto.email);
+      if (emailExist) {
+        throw new ConflictException(EXCEPTIONS.Conflict);
+      }
+    }
+
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, 8);
+    }
+
+    const modifiedUserDto: User = {
+      ...currentUser,
+      username: updateUserDto?.username,
+      avatar: updateUserDto?.avatar,
+      email: updateUserDto?.email,
+      password: updateUserDto?.password,
+      about: updateUserDto?.about,
+    };
+
+    await this.userRepository.update(currentUser.id, modifiedUserDto);
+    return await this.findOne(id);
   }
 }
