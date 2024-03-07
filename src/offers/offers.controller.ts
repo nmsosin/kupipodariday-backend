f8232c84ef1b8b1f6eb88dc60ed94f0e56242e00
Nikolay Sosin <nmsosin@yandex.ru>
2024-03-07 20:15:37 +0400

    feat: complete offers controller

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -1,34 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { OffersService } from './offers.service';
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { UpdateOfferDto } from './dto/update-offer.dto';
+import { User } from '../users/entities/user.entity';
+import { JwtGuard } from '../auth/guards/jwt.guard';
 
 @Controller('offers')
 export class OffersController {
   constructor(private readonly offersService: OffersService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
-  create(@Body() createOfferDto: CreateOfferDto) {
-    return this.offersService.create(createOfferDto);
+  create(
+    @Body() createOfferDto: CreateOfferDto,
+    @Req() { user }: { user: User },
+  ) {
+    return this.offersService.create(createOfferDto, user);
   }
 
+  @UseGuards(JwtGuard)
   @Get()
   findAll() {
     return this.offersService.findAll();
   }
 
+  @UseGuards(JwtGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.offersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOfferDto: UpdateOfferDto) {
-    return this.offersService.update(+id, updateOfferDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.offersService.remove(+id);
   }
 }
